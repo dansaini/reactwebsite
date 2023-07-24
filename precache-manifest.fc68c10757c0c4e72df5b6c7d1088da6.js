42b6239ce2dfc4a063d03a55aa93d5fd15f0349c
dansaini <dan.saini@minddigital.com>
2023-07-24 12:23:50 +0530

    Updates

---
+++ precache-manifest.fc68c10757c0c4e72df5b6c7d1088da6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e410ea496a3fd036583c",
+    "url": "/reactwebsite/static/css/main.c6ad3df5.chunk.css"
+  },
+  {
+    "revision": "e410ea496a3fd036583c",
+    "url": "/reactwebsite/static/js/main.e410ea49.chunk.js"
+  },
+  {
+    "revision": "ff3e70574e23ec862a62",
+    "url": "/reactwebsite/static/js/1.ff3e7057.chunk.js"
+  },
+  {
+    "revision": "3ccc09efa27abe7d0767",
+    "url": "/reactwebsite/static/css/2.63b0ab46.chunk.css"
+  },
+  {
+    "revision": "3ccc09efa27abe7d0767",
+    "url": "/reactwebsite/static/js/2.3ccc09ef.chunk.js"
+  },
+  {
+    "revision": "c103eb179fbceb8828fa",
+    "url": "/reactwebsite/static/js/runtime~main.c103eb17.js"
+  },
+  {
+    "revision": "5e6c109cbca631c48c4369fbf985cf5a",
+    "url": "/reactwebsite/index.html"
+  }
+];
